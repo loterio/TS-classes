f7bd730738dad5e65ff239bddc3eb1f4042fcf35
loterio <vitorloterio@gmail.com>
2020-06-22 15:12:12 -0300

    created Person class

--- dist/person.js
+++ dist/person.js
@@ -1,5 +1,17 @@
 var Person = /** @class */ (function () {
     function Person() {
     }
+    Person.prototype.presentation = function () {
+        var fullName = this.firstName + " " + this.lastName;
+        console.log("my name is " + fullName + "\n                    i was born on " + this.bornDate + "\n                    and my identification references are:\n                    CPF: " + this.cpf + " and RG: " + this.rg);
+    };
     return Person;
 }());
+var me = new Person();
+me.firstName = "Fabio";
+me.lastName = "Loterio";
+me.cpf = 07;
+944031907;
+me.rg = 6936123;
+me.bornDate = 28072002;
+me.presentation();
