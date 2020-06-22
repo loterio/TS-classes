5d95dff54c490d98ddaa86b1aa64d594b09a466a
loterio <vitorloterio@gmail.com>
2020-06-22 15:44:56 -0300

    finished bike class

--- bike.ts
+++ bike.ts
@@ -6,4 +6,33 @@ class Bicicle{ // Bicicleta (Marca, modelo, tipo (passeio, mtb ou speed), tamanh
     bodySize: number;
     numGears: number;
     suspension: boolean;
+
+    description():void{
+        console.log(`
+        a ${this.model} made by ${this.fabricant}.\n
+        this bike have ${this.numGears} marchs. 
+        `);
+    }
+    fullDescription():void{
+        console.log(`
+        models: ${this.model}\n 
+        fabricant: ${this.fabricant}\n
+        number of gears: ${this.numGears}\n
+        wheel size: ${this.wheelSize} mm\n
+        body size: ${this.bodySize} mm\n
+        have suspension? ${this.suspension}\n
+        it's recomendable use it for ${this.kind} style of cycling. 
+        `);
+    }
 }
+
+let m = new Bicicle();
+m.fabricant = "mormaii";
+m.model = "Urban Sense";
+m.kind = "speed";
+m.wheelSize = 14;
+m.bodySize = 700;
+m.numGears = 36;
+m.suspension = true;
+m.description();
+m.fullDescription();
--- dist/bike.js
+++ dist/bike.js
@@ -1,5 +1,21 @@
 var Bicicle = /** @class */ (function () {
     function Bicicle() {
     }
+    Bicicle.prototype.description = function () {
+        console.log("\n        a " + this.model + " made by " + this.fabricant + ".\n\n        this bike have " + this.numGears + " marchs. \n        ");
+    };
+    Bicicle.prototype.fullDescription = function () {
+        console.log("\n        models: " + this.model + "\n \n        fabricant: " + this.fabricant + "\n\n        number of gears: " + this.numGears + "\n\n        wheel size: " + this.wheelSize + " mm\n\n        body size: " + this.bodySize + " mm\n\n        have suspension? " + this.suspension + "\n\n        it's recomendable use it for " + this.kind + " style of cycling. \n        ");
+    };
     return Bicicle;
 }());
+var m = new Bicicle();
+m.fabricant = "mormaii";
+m.model = "Urban Sense";
+m.kind = "speed";
+m.wheelSize = 14;
+m.bodySize = 700;
+m.numGears = 36;
+m.suspension = true;
+m.description();
+m.fullDescription();
