911bdff1e704c863111af83154f14ac824e7e03f
loterio <vitorloterio@gmail.com>
2020-06-22 15:12:48 -0300

    created Person class

--- person.ts
+++ person.ts
@@ -4,4 +4,20 @@ class Person { // Pessoa (nome, sobrenome, CPF, RG, data nascimento)
     cpf: number;
     rg: number;
     bornDate: number;
+
+    presentation():void {
+        let fullName = this.firstName + " " + this.lastName;
+        console.log(`my name is ${fullName}
+                    i was born on ${this.bornDate}
+                    and my identification references are:
+                    CPF: ${this.cpf} and RG: ${this.rg}`);
+    }
 }
+
+let me = new Person();
+me.firstName = "Fabio"
+me.lastName = "Loterio"
+me.cpf = 07;944031907
+me.rg = 6936123
+me.bornDate = 28072002
+me.presentation();
